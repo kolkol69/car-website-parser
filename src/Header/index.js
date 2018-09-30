e0259dfce02abeb9ca3ba900772dc37592e7a189
maksym kolodiy <maksym.kolodiy.intern@3ds.com>
2018-09-30 21:05:14 +0200

    updated with hamburger menu

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,24 +1,26 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import { Navbar, FormGroup, FormControl, Button } from 'react-bootstrap'
+import { Navbar } from 'react-bootstrap'
+import { Nav, MenuItem, NavDropdown, NavItem } from 'react-bootstrap'
 import './style.css'
 
 export default class Header extends Component {
   render() {
     return (
-      <Navbar>
+      <Navbar collapseOnSelect>
         <Navbar.Header>
-          <Navbar.Brand>
-            <a href="/cars">RST.UA</a>
-          </Navbar.Brand>
+        <Navbar.Brand>
+      <a href="/cars">RST.UA</a>
+    </Navbar.Brand>
+          <Navbar.Toggle />
         </Navbar.Header>
         <Navbar.Collapse>
           <Navbar.Text>
-            <Link className='nav-item' to='/'>Home</Link>
-          </Navbar.Text >
-          <Navbar.Text>
-            <Link className='nav-item' to='/cars'>Cars</Link>
-          </Navbar.Text >
+      <Link className='nav-item' to='/'>Home</Link>
+    </Navbar.Text >
+    <Navbar.Text>
+      <Link className='nav-item' to='/cars'>Cars</Link>
+    </Navbar.Text >
         </Navbar.Collapse>
       </Navbar>
     )
