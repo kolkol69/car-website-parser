ea244da30d3121de133ecc7b58483256fd9f74a4
maksym kolodiy <maksym.kolodiy.intern@3ds.com>
2018-09-30 13:35:59 +0200

    styled components are inside of folders /w index and style

--- src/CarsList/index.js
+++ src/CarsList/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, Redirect } from 'react-router-dom'
 import { ListGroup, ListGroupItem, Image } from 'react-bootstrap'
 import './style.css'
 
@@ -7,15 +7,19 @@ export default class CarsList extends Component {
   render() {
     const carAPI = this.props.carAPI;
     return (
-      <ListGroup>
+      <ListGroup className='row list-unstyled'>
         {
-          carAPI.map(car => (
+          carAPI.map((car,i) => (
             <ListGroupItem
               key={car.id}
+              className={`${i%2==0?'col-md-6 li-background':'col-md-6'}`}
               header={<Link style={{textDecoration: 'none'}} className='header-link' to={`/cars/${car.number}`}>{car.title}</Link>}
+              // header={car.title}  
+              // onClick={alert('hit')}
             >
-              <Link to={`/cars/${car.number}`}><Image src={car.img}  /></Link>
-              <span style={{float: 'right'}}>
+              <Link  to={`/cars/${car.number}`}><Image src={car.img}  /></Link>
+              {/* <Link className='col-md-8' to={`/cars/${car.number}`}><Image src={car.img}  /></Link> */}
+              <span >
                 Price: {car.price} <br/>
                 Year: {car.year}
               </span>
