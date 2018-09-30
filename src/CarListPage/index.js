07f65aa42374841137346fe41caf80f727d8f5be
maksym kolodiy <maksym.kolodiy.intern@3ds.com>
2018-09-30 18:20:46 +0200

    minor changes

--- src/CarListPage/index.js
+++ src/CarListPage/index.js
@@ -2,9 +2,6 @@ import React, { Component } from 'react'
 import CarsFilteredList from '../CarsFilteredList'
 
 export default class CarListPage extends Component {
-    constructor(props) {
-        super(props);
-    }
     render() {
         return (
             <div>
